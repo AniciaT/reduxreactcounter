3b8a32f845506c85cbce763774b0c98689a8c97e
Anicia Tarlao <tarlaoanicia@gmail.com>
2019-12-05 15:23:54 +0100

    first commi

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import counterReducer from './counterReducer';
+import CounterContainer from './CounterContainer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+        <CounterContainer />
+    </Provider>,
+    document.getElementById("root")
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
